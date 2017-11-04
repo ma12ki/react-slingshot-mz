27772d8b34dd292818596ea2f56cc51ae5095580
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2017-11-04 23:42:25 +0100

    working comments

--- src/modules/blog/components/CommentList/CommentList.js
+++ src/modules/blog/components/CommentList/CommentList.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Link from 'redux-first-router-link';
 
-const CommentList = () => <div>lol comments</div>;
+import { getComments } from '../../selectors';
 
-export default CommentList;
+const CommentList = ({ comments, postId }) => {
+  const commentNodes = comments.map(({ id, name, email, body }) => (
+    <div key={id}>
+      <h4>{name} ({email})</h4>
+      <p>{body}</p>
+    </div>
+  ));
+
+  return (
+    <div>
+      <Link to={`/posts/${postId}`}>back to post</Link>
+      {commentNodes}
+    </div>
+  );
+};
+
+CommentList.propTypes = {
+  comments: PropTypes.array.isRequired,
+  postId: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  comments: getComments(state),
+  postId: state.location.payload.id,
+});
+
+export default connect(mapStateToProps)(CommentList);
--- src/modules/blog/selectors.js
+++ src/modules/blog/selectors.js
@@ -4,7 +4,7 @@ const getBlog = (state) => state[moduleName];
 
 const getPosts = (state) => getBlog(state).posts;
 const getPost = (state) => getBlog(state).post;
-const getComments = (state) => getComments(state).comments;
+const getComments = (state) => getBlog(state).comments;
 
 export {
   getPosts,
