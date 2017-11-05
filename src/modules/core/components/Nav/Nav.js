31d649830e99cd91901b13664f75180e6d12f2a8
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2017-11-05 21:36:35 +0100

    simplified param

--- src/modules/core/components/Nav/Nav.js
+++ src/modules/core/components/Nav/Nav.js
@@ -6,7 +6,7 @@ const activeStyle = { fontWeight: 'bold', textDecoration: 'underline' };
 const Nav = () => {
   return (
     <ul>
-      <li><NavLink to={'/'} activeStyle={activeStyle} exact={true}>Home</NavLink></li>
+      <li><NavLink to={'/'} activeStyle={activeStyle} exact>Home</NavLink></li>
       <li><NavLink to={'/posts'} activeStyle={activeStyle}>Blog (thunks)</NavLink></li>
       <li><NavLink to={'/nested'} activeStyle={activeStyle}>Nested routes</NavLink></li>
       <li><a href="/something-that-does-not-exist">404</a></li>
