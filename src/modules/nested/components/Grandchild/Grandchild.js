60e4b7660e578291221f6e291ff1cc245ac91c29
Marcin Zolkiewski <zolkiewski.marcin@gmail.com>
2017-11-05 16:42:44 +0100

    added componentDidMount to nested routes

--- src/modules/nested/components/Grandchild/Grandchild.js
+++ src/modules/nested/components/Grandchild/Grandchild.js
@@ -5,16 +5,35 @@ import Link from 'redux-first-router-link';
 
 import { getGrandchild } from '../../selectors';
 
-const Grandchild = ({ name }) => {
-  return (
-    <div>
-      <h3>I'm the grandchild and my name is {name}</h3>
-      <Link to={`/nested`}>show only the parent</Link>
-      <br />
-      <Link to={`/nested/someChild`}>show child</Link>
-    </div>
-  );
-};
+// const Grandchild = ({ name }) => {
+//   return (
+//     <div>
+//       <h3>I'm the grandchild and my name is {name}</h3>
+//       <Link to={`/nested`}>show only the parent</Link>
+//       <br />
+//       <Link to={`/nested/someChild`}>show child</Link>
+//     </div>
+//   );
+// };
+
+class Grandchild extends React.Component {
+
+  componentDidMount() {
+    console.log('Grandchild mounted');
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        <h3>I'm the grandchild and my name is {name}</h3>
+        <Link to={`/nested`}>show only the parent</Link>
+        <br />
+        <Link to={`/nested/someChild`}>show child</Link>
+      </div>
+    );
+  }
+}
 
 Grandchild.propTypes = {
   name: PropTypes.string,
